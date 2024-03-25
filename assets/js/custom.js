ee7b0b562764840924398c1a49ba3d5aeb167715
meenaratha <meenatchipkr@gmail.com>
2024-03-25 23:10:00 +0530

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,30 @@
+document.getElementById('searchInput').addEventListener('keyup', function() {
+    var icon = document.getElementById('searchIcon');
+    if (this.value) {
+        icon.style.display = 'none';
+    } else {
+        icon.style.display = 'block';
+    }
+});
+
+
+const tabs = document.querySelectorAll('.tab-button');
+const all_content = document.querySelectorAll('.tab-content-box');
+
+tabs.forEach((tab,index)=>{
+    tab.addEventListener('click', (e)=>{
+        tabs.forEach(tab=>{tab.classList.remove('active')});
+        tab.classList.add('active');
+        var line = document.querySelector('.line');
+        line.style.width = e.target.offsetWidth + "px" ;
+        line.style.left = e.target.offsetLeft + "px" ;
+
+        all_content.forEach(content=>{
+            content.classList.remove('active')
+        });
+        all_content[index].classList.add('active');
+    })
+
+   
+
+})
